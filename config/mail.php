30bcdff30c28ed8c1b7abcf232bfa88320524c58
tasoavci <tasoavci2002@gmail.com>
2026-01-12 20:08:49 +0300

    mail timeout

--- config/mail.php
+++ config/mail.php
@@ -46,7 +46,7 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
+            'timeout' => 5,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
         
